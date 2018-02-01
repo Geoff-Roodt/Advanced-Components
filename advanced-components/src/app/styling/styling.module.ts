64da0e859633b915e916e17b9491d37802418fef
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-02-02 12:11:24 +1300

    Added styling module and inline style component

---
+++ advanced-components/src/app/styling/styling.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InlineStyleComponent } from './inline-style/inline-style.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    InlineStyleComponent
+  ]
+})
+
+export class StylingModule { }
